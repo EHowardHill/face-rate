313e6f824cb032cc4349397b65e3655c5513e964
Ethan Hill <ethan.hill1999@outlook.com>
2022-03-25 04:51:24 -0500

    new driver maybe?

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://ethan:Shebang01#!@face-server.database.windows.net:1433/db?driver=SQL+Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://ethan:Shebang01#!@face-server.database.windows.net:1433/db?driver=ODBC+Driver+17+for+SQL+Server'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
